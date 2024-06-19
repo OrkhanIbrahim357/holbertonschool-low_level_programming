c800ba57d9c4ef6d251f581ec757e8b14621fcad
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-06-19 12:13:48 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * * main - Prints "with proper grammar, but the outcome is
+ * main - Prints "with proper grammar, but the outcome is
  * a piece of art," followed by a new line.
  * Return:Always 0
  */
