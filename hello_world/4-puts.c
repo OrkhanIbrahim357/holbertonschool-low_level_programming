d09483cbcb74c745f6adf7810f959c681feac3d7
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-06-19 08:36:34 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line 
  * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+
+	  puts("\"Programming is like building a multilingual puzzle");
+          return (0);
+
 }
