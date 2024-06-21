1c1ef15ca05913cf43533f13ded9b575f5e32284
Orkhanibrahim357 <Orkhan_irbahim@mail.ru>
2024-06-21 11:21:36 +0000

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+int main(void)
+{
+	int no ;
+	printf("enter random number\n");
+	scanf("%d", &n);
+	if(n>0)
+	{
+		printf(("number is positive");
+	}
+	if(no<0)
+        {
+         	 printf("n is negative");
+	}
+	if(no==0)
+	{
+	         printf("number is zero")
+		 }
+}
