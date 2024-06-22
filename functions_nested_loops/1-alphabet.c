6c407a122230f9df58b8230fa32fd0fa50cfdbae
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-22 22:47:39 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,29 +6,13 @@
  */
 void print_alphabet(void)
 {
-    char letter = 'a';  // Start with 'a', the first lowercase letter
-    while (letter <= 'z')
-    {
-        _putchar(letter);  // Print the current letter
-        letter++;          // Move to the next letter in the alphabet
-    }
-    _putchar('\n');        // Print a new line after printing all letters
-}
+char letter = 'a';
 
-------------------------------------
-
-vim 1-main.c
-
-/* File: 1-main.c */
-#include "main.h"
-
-/**
- * main - Calls the print_alphabet function.
- *
- * Return: Always 0.
- */
-int main(void)
+while (letter <= 'z')
 {
-    print_alphabet();
-    return (0);
+_putchar(letter);
+letter++;
 }
+_putchar('\n');
+}
+
