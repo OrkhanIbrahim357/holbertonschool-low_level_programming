edd01bc12da63ef1e083fa22fb0e91e9e7e15820
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-22 07:53:51 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - True
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
