6a0b84270ac26e82bd2b239b4bf0fb327877d807
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-22 07:57:35 +0000

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -True
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
