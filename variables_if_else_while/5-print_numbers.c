a24090876e67d08a7cb4819d847776a9db0a58ed
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-22 08:00:12 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - True
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
