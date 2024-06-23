858083f36d99d73baf11d8a97b7a27fb50c04908
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-23 19:42:28 +0000

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
+// main.h
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet_x10(void);
-int _putchar(char c); // Function prototype for _putchar
+int _putchar(char c); /* Function prototype for _putchar */
 
 #endif /* MAIN_H */
