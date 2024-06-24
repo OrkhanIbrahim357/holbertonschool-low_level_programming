87c8cc85f9a819825a8c1f253cea7442f323a2e4
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-06-24 12:29:53 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - main
+ * @c: var 1
+ *
+ * Return: zero
+ */
+int _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
