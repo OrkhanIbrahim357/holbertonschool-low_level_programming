fe65e95bec21e2237bc703936b61a3a02680f017
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-25 15:42:22 +0000

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void);
+{
+	int  eded;
+
+	for (eded = 0; eded <= 9; eded++)
+		_putchar(eded + '0');
+	_putchar('\n');
+}
