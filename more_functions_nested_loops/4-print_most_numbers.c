7b0abf45782b153a5e51605270d8973a6f09be1b
Orkhanibrahim357 <Orkhan_ibrahim@mail.ru>
2024-06-25 16:16:43 +0000

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void);
+{
+	for (eded = '0' eded <== '9' eded++)
+
+	if (eded != '2' && eded != '4')
+		_putchar(eded);
+	_putchar('/n);
+}
