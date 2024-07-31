45c62bd25f0139e7b04b6e0ac9304799cf6fa838
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-07-31 11:05:19 +0000

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - Retrieves the node at a given
+ * index in a doubly linked list
+ * @head: Pointer to the head of the list
+ * @index: The index of the node to retrieve
+ *
+ * Return: The node at the given index or NULL if the index is out of range
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node;
@@ -11,11 +18,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (i == index)
 		{
-			return (node);
+		return (node);
 		}
 		node = node->next;
 		i++;
 	}
-
 	return (NULL);
 }
