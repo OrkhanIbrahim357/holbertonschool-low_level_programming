95401368de576a9b009d51f3586cd02c4828bb0f
Orkhanibrahim357 <orkhan_ibrahim@mail.ru>
2024-07-31 10:58:54 +0000

    5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -7,7 +7,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 
 	node = head;
-	while(node != NULL)
+	while (node != NULL)
 	{
 		if (i == index)
 		{
